c864dab5dbde964611511a2d0536e326fdb875e2
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-14 19:09:35 -0700

    Update String-transformer.js

--- String-transformer.js
+++ String-transformer.js
@@ -27,3 +27,16 @@ function stringTransformer(str) {
 }
 
 // or
+
+function stringTransformer(str) {
+  let words = str.split(' ').reverse();
+  let chars = words.join(' ').split('');
+  let returnString = '';
+  
+  chars.forEach((char) => {
+    returnString += (char == char.toUpperCase()) ? 
+      char.toLowerCase() : char.toUpperCase();
+  });
+  
+  return returnString;
+}
