972e8ed702aaa301b99d11c66bd6d597daf4fc91
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-03-14 19:08:03 -0700

    Update String-transformer.js

--- String-transformer.js
+++ String-transformer.js
@@ -13,3 +13,15 @@ For example:
 
 
 // Solution
+
+function stringTransformer(str) {
+  return str
+    .split(' ')
+    .reverse()
+    .join(' ')
+    .split('')
+    .map(v => v == v.toUpperCase() ?
+      v.toLowerCase() :
+      v.toUpperCase())
+    .join('');
+}
